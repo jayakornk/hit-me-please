45ffb8ace431f1f1a4afda7d74abf6f590e9dc09
prontobacon <bacon@prontomarketing.com>
2019-06-02 10:23:09 +0700

    🔧 Create one simple compute instance using Terraform 🥓

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,30 @@ provider "google" {
   zone        = "asia-southeast1-b"
 }
 
+resource "google_compute_instance" "tf_instance" {
+  name         = "tf-instance-${count.index}"
+  machine_type = "n1-standard-1"
+  count = 1
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20190530"
+      size  = 30
+    }
+  }
+
+  metadata = {
+    sshKeys = "bacon:${file("~/.ssh/id_rsa_work.pub")}"
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  tags = ["my-web"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.tf_instance.*.network_interface.0.access_config.0.nat_ip}"
+}
